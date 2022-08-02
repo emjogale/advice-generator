ef303f38bdaf042f47d8afca0c430357988b4eb2
Emma Gale <emmajgale@hotmail.com>
2022-08-02 18:28:13 +0100

    add in styles and define variables for functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const dice_btn = document.getElementById("dice");
+
+const advice_number = document.getElementById("advice-number");
+
+const advice = document.getElementById("advice");
+console.log(advice.textContent);
