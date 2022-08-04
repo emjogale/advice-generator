9338e42070424d64964af69ab7b0e258b2f821b4
Emma Gale <emmajgale@hotmail.com>
2022-08-04 14:27:11 +0100

    add in functionality for Advice Slip api

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+// define variables
 const dice_btn = document.getElementById("dice");
+const advice_numberEL = document.getElementById("advice-number");
+const adviceEL = document.getElementById("advice");
 
-const advice_number = document.getElementById("advice-number");
+// fetch advice and update the DOM
+function getAdvice() {
+	fetch(`https://api.adviceslip.com/advice`).then((res) =>
+		res.json().then((data) => {
+			advice_numberEL.innerText = data.slip.id;
+			advice.textContent = data.slip.advice;
+		})
+	);
+}
 
-const advice = document.getElementById("advice");
-console.log(advice.textContent);
+// event listener
+dice_btn.addEventListener("click", getAdvice);
